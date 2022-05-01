2481bfe4090809ee419873343fba7197614a0952
Виктор <itsmeviktor@mail.ru>
2022-05-01 16:39:51 +0300

    finish with setting tableview

--- TheListOfСharacter/AmiboListVC.swift
+++ TheListOfСharacter/AmiboListVC.swift
@@ -16,8 +16,12 @@ class AmiboListVC: UIViewController {
         view.backgroundColor = .white
         safeArea = view.layoutMarginsGuide
         
+        tableView.dataSource = self
+        
         setupView ()
     }
+    
+    //MARK: - Setup view
     
     func setupView () {
         //always add the UIview FIRST before setting constarains
@@ -29,6 +33,23 @@ class AmiboListVC: UIViewController {
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
-        
     }
 }
+
+// MARK: - UiTableViewDataSoucrce
+
+extension AmiboListVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = UITableViewCell ()
+        cell.textLabel?.text = "Hey! listen!"
+        return cell
+    }
+
+}
+
+
